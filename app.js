700858fc3176d232d1d7f672de084877bcd37b27
Shivsharan yadav <shivsharanyadav9516@gmail.com>
2026-02-22 12:28:23 +0530

    first commit

--- app.js
+++ app.js
@@ -1,33 +1,24 @@
-
+// Core Modules
 const path = require("path");
 
+// External Module
 const express = require("express");
 const bodyParser = require("body-parser");
-const {hostRouter} = require("./routers/hostRouter");
+
+// Local Module
+const { hostRouter } = require("./routers/hostRouter");
 const storeRouter = require("./routers/storeRouter");
-
 const rootDir = require("./util/path-util");
-const errorController = require("./controllers/errorController");
+const errorController = require('./controllers/errorController');
+
 const app = express();
-
-
 app.set('view engine', 'ejs');
-
-app.set('views',  'views');
+app.set('views', 'views');
 
 app.use(express.static(path.join(rootDir, "public")));
-
-// Middleware to parse URL-encoded bodies
-app.use(express.urlencoded({ extended: true }));
-
-// Use routers
-app.use(storeRouter); 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(storeRouter);
 app.use("/host", hostRouter);
-
-app.use((req, res, next) => {
-  console.log("Request received", req.url, req.method);
-  next();
-});
 
 app.use(errorController.get404);
 
